7b5d1feda5ab52774f28dfcf55b9978b65c0178e
si-u-gi <ds10163@gmail.com>
2025-11-11 10:21:15 +0000

    11강-5(Override/Implement Methods 해결)

--- hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -1,5 +1,34 @@
 package hello.hello_spring.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import hello.hello_spring.domain.Member;
+
 public class MemoryMemberRepository implements MemberRepository {
+
+    @Override
+    public List<Member> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
+
+    @Override
+    public Member save(Member member) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
